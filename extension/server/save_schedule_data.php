0e85b1d1e936ca3ce76edd43eb8a6d55ba67771a
Huuuunt <fzfjchina@gmail.com>
2013-01-07 22:17:59 +0800

    extension server

---
+++ extension/server/save_schedule_data.php
@@ -0,0 +1,25 @@
+<?php
+
+# 允许跨域访问
+header("Access-Control-Allow-Origin: *"); 
+
+# 获取足球赛程数据
+$schedule = $_POST["schedule"];
+$match = $_POST["match"];
+$season = $_POST["season"];
+$phase = $_POST["phase"];
+
+/*
+$logFileHandle = fopen("/usr/share/nginx/www/visitLog.log", "w+");
+fwrite($logFileHandle, $schedule);
+fclose($logFileHandle);
+*/
+
+//$datafile = "/home/liuf/github/huuuunt/app/data/schedule/gooooal/2007/1/" . $phase;
+$datafile = "/usr/share/nginx/www/gooooal/" . $season . "/" . $match . "/" . $phase;
+$logFileHandle = fopen($datafile, "w+");
+fwrite($logFileHandle, $schedule);
+fclose($logFileHandle);
+
+?>
+
